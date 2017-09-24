d65fae3cbaf34d72af327b22e96ff2c26d306aa7
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-09-24 14:42:35 +0530

    implemented less

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import './style/index.less'
 
 // main routes
 import AppRoutes from './routes';
--- routes.js
+++ routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import App from './containers/App';
-import About from './containers/About';
+import {Home} from './containers/';
 
 export default () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-			  <Route exact path='/' component={App}/>
-			  <Route path='/about' component={About}/>
+			  <Route exact path='/' component={Home}/>
 			</Switch>
 		</BrowserRouter>
 	)
