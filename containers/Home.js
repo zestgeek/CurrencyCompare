2f3f9bd1d2254f6b0f2935b0d4e76d9fd8ad4e4d
Manoj Singh Negi <me@manojsinghnegi.com>
2017-09-26 01:21:09 +0530

    removed chart for now

--- containers/Home.js
+++ containers/Home.js
@@ -6,14 +6,6 @@ import {
 	Col
 } from 'elemental';
 
-import {
-	AreaChart,
-	Area,
-	CartesianGrid,
-	XAxis,
-	YAxis
-} from 'recharts';
-
 import Select from 'react-select';
 
 import * as currencyActions from '../actions/currency';
@@ -42,16 +34,6 @@ class Home extends React.Component {
 
 	render () {
 
-		const data = [
-	      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-	      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-	      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-	      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-	      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-	      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-	      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
-		];
-
 		const {rates} = this.props.currencyReducer;
 
 		const newoptions = Countries.map(country => {
@@ -70,18 +52,9 @@ class Home extends React.Component {
 			}
 
 		})
-		
+
 		return (
 			<div>
-				<InCenter>
-					<h1 className="text-center">Currency Compare</h1>
-					<AreaChart width={600} height={300} data={data}>
-					  <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
-					  <CartesianGrid stroke="#ccc" />
-					  <XAxis dataKey="name" />
-					  <YAxis />
-					</AreaChart>
-				</InCenter>
 				<Col>
 					<Row>
 						<Col sm="1/3"></Col>
